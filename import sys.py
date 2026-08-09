9d4e773ff6ce80ad6c6d077cf3235663705ad9e2
moayad alomar <mwydalmr2@gmail.com>
2026-08-09 20:43:49 +0300

    new 2

---
+++ import sys.py
@@ -0,0 +1,24 @@
+# import sys
+# sys.path.append(r"home/ubuntu/mm")
+# print(sys.path)
+# import momo
+# # print (dir (momo))
+# momo.seyHallo("moayad")
+# momo.seyHallo("ahmad")
+# momo.seyHallo("osama")
+  
+
+# momo.seyHawAreYou("moayad")
+# momo.seyHawAreYou("ahmad")
+# momo.seyHawAreYou("osama")
+# import momo as ff
+# ff.seyHallo("moayad")
+# ff.seyHawAreYou("moayad")
+
+# ___________________________________________
+# from momo import seyHallo
+# seyHallo("moayad")
+#  ++++++++++++++++++++
+from momo import seyHallo as ss
+
+ss("moayad")
---
+++ momo.py
@@ -0,0 +1,4 @@
+def seyHallo(name):
+    print(f"Hallo, {name}")
+def seyHawAreYou(name):
+    print(f"How are you, {name} ?")
